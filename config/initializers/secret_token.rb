66da9ab8643664429d78b1a1ea4c1a3f3888d72d
Peter Hamilton <peter@inspiredpixel.net>
2012-07-20 20:33:51 -0700

    Initial Commit - Blank Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonTemplate::Application.config.secret_token = '3e1c12611a514d1ad778a5ab1731f1f2c552e8f4fcfcfcfd8bf941aebcabacde4aae11c46b545f7de80d135973af48bfe9da840d3145c26ccfb412c7148b7bd5'
